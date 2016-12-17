63ee46268e4761807a50c125f9ae1c06117bc0f8
Steven Nunez <steven.nunez@gmail.com>
2016-12-16 19:08:26 -0500

    Silly Beef Example

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,21 +1,16 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
+import { createStore } from 'redux'
 
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
+function beef(state=""){ 
+  return `${state} BEEEF`
+}
+const store = createStore(beef)
+store.subscribe(() => {
+  console.log("This is being called in a subscribe")
+  console.log("The current state is ", store.getState())
+})
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
+let action = {type: "LALALALA"}
+store.dispatch(action)
+store.dispatch(action)
+store.dispatch(action)
+store.dispatch(action)
